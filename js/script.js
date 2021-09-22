f7f911de871c2053742e28c43ca3eaec14f4a07c
fomes <contato.felipeluna@gmail.com>
2021-09-22 15:16:35 -0300

    fix layout footer

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const artist = $('#artist');
 const song = $('#song');
 const lyric = $('#lyric');
 const form = $('#my-form');
+const footer = $('.footer');
 
 function getLyrics(artist, title) {
   return fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`);
@@ -25,20 +26,23 @@ clear.addEventListener('click', () => {
 async function showLyrics() {
   lyric.innerHTML = '<div class="spinner-border" role="status"><span class="sr-only">Loading...</span></div>';
 
+  if (lyric.innerHTML) {
+    footer.style.display = 'none';
+  }
+
   try {
     const resp = await getLyrics(artist.value, song.value);
     const data = await resp.json();
-
     const songs = JSON.parse(localStorage.getItem('lyrics')) || [];
-
+    
     if(data.lyrics) {
       if(songs.find(e => e.artist === artist.value) && songs.find(e => e.song === song.value)) {
         lyric.innerHTML = songs.filter(e => e.artist === artist.value).filter(e => e.song === song.value).map(e => e.lyric);
-
+        
       } else {
         const newSong = {'artist' : artist.value, 'song': song.value, 'lyric': data.lyrics};
         songs.push(newSong);
-
+        
         lyric.innerHTML = songs.filter(e => e.artist === artist.value).filter(e => e.song === song.value).map(e => e.lyric);
         localStorage.setItem('lyrics', JSON.stringify(songs))
       }
